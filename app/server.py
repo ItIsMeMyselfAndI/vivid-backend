865e468e6dd11a6b7c336ae112bf6ad566f8b7c1
itismemyselfandi <egermirasol2023@gmail.com>
2025-12-26 20:56:35 +0800

    chore: updating allowed origins

--- app/server.py
+++ app/server.py
@@ -5,7 +5,7 @@ from schema.settings import CreateSettings, UpdateSettings
 from schema.history import CreateHistory, UpdateHistory
 from client.index import supabase
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
 import os
 from dotenv import load_dotenv
@@ -13,6 +13,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 project_url = os.environ.get("PROJECT_URL")
+print(project_url)
 if not project_url:
     print("[Exit] PROJECT_URL doesn't exist")
     exit(0)
@@ -20,8 +21,17 @@ if not project_url:
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    # allow_origins=[project_url],
-    allow_origins=["*"],
+    allow_origins=[
+        project_url,
+        f"{project_url}/dashboard",
+        f"{project_url}/simulations",
+        f"{project_url}/simulations/stack",
+        f"{project_url}/simulations/queue",
+        f"{project_url}/simulations/binary-tree",
+        f"{project_url}/simulations/binary-search-tree",
+        f"{project_url}/simulations/recursion",
+        # "*",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -31,25 +41,27 @@ app.add_middleware(
 # ----- simulation endpoints -----
 
 @app.get("/api/get-simulation")
-def get_simulation(user_id: str, simulation_type: SimulationType):
+def get_simulation(user_id: str, simulation_type: SimulationType, req: Request):
     response = supabase.table("simulation").select("*").match(
         {"user_id": user_id, "type": simulation_type.value}
     ).execute()
     print(response)
+    print(req)
     return response
 
 
 @app.post("/api/create-simulation")
-def create_simulation(data:  CreateSimulation):
+def create_simulation(data:  CreateSimulation, req: Request):
     response = supabase.table("simulation").insert(
         data.model_dump(mode="json")).execute()
     print(response)
+    print(req)
     return response
 
 
 @app.put("/api/update-simulation")
 def update_simulation(
-        user_id: str, simulation_type: SimulationType, data:  UpdateSimulation
+        user_id: str, simulation_type: SimulationType, data:  UpdateSimulation, req: Request
 ):
     response = supabase.table("simulation").update(
         data.model_dump(mode="json", exclude_none=True)
@@ -61,7 +73,7 @@ def update_simulation(
 # ----- history endpoints -----
 
 @app.get("/api/get-histories")
-def get_histories(user_id: str, count: int):
+def get_histories(user_id: str, count: int, req: Request):
     response = supabase.table("history").select("*").match(
         {"user_id": user_id}
     ).order("created_at", desc=True).limit(count).execute()
@@ -70,7 +82,7 @@ def get_histories(user_id: str, count: int):
 
 
 @app.post("/api/create-history")
-def create_history(data:  CreateHistory):
+def create_history(data:  CreateHistory, req: Request):
     response = supabase.table("history").insert(
         data.model_dump(mode="json")).execute()
     print(response)
@@ -79,7 +91,7 @@ def create_history(data:  CreateHistory):
 
 @app.put("/api/update-history")
 def update_history(
-        user_id: str, history_id: int, data:  UpdateHistory
+        user_id: str, history_id: int, data:  UpdateHistory, req: Request
 ):
     response = supabase.table("history").update(
         data.model_dump(mode="json", exclude_none=True)
@@ -91,7 +103,7 @@ def update_history(
 # ----- settings endpoints -----
 
 @app.get("/api/get-settings")
-def get_settingss(user_id: str):
+def get_settingss(user_id: str, req: Request):
     response = supabase.table("settings").select("*").match(
         {"user_id": user_id}).execute()
     print(response)
@@ -99,7 +111,7 @@ def get_settingss(user_id: str):
 
 
 @app.post("/api/create-settings")
-def create_settings(data:  CreateSettings):
+def create_settings(data:  CreateSettings, req: Request):
     response = supabase.table("settings").insert(
         data.model_dump(mode="json")).execute()
     print(response)
@@ -108,7 +120,7 @@ def create_settings(data:  CreateSettings):
 
 @app.put("/api/update-settings")
 def update_settings(
-        user_id: str, data:  UpdateSettings
+        user_id: str, data:  UpdateSettings, req: Request
 ):
     response = supabase.table("settings").update(
         data.model_dump(mode="json", exclude_none=True)
